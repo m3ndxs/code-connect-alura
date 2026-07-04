0846a214c434f60df1fd5d5dde4b3d52acdbfc69
Leonardo Mendes <leonardo_mendes2002@hotmail.com>
2026-07-04 13:55:02 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/code_connect_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.code_connect_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Code Connect',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Code Connect')));
+  }
+}
